812ef6b566fddb52c5b7abd1f5d440dfbe36754f
sujithk-kl <sujithkumaravel03@gmail.com>
2025-08-25 14:46:53 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,6 @@ app.use((err, req, res, next) => {
 app.use((req, res) => {
     res.status(404).json({ error: 'Route not found' });
 });
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
